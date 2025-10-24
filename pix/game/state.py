18dd7286ea5d8cccafed75993f5e6c095050227f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-10-24 19:23:20 +0200

    Animate an element

---
+++ pix/game/state.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from pix.sprite import Sprite
+
+class GameState:
+    def __init__(self):
+        self.element: Optional[Element] = None
+
+    def tick(self):
+        self.element.tick()
+
+class Element:
+    def __init__(self, sprite: Sprite):
+        self.__sprite = sprite
+        self.__initiated = False
+        self.__frame = 0
+
+    def initiate(self) -> None:
+        self.__initiated = True
+
+    def tick(self) -> None:
+        if self.__initiated:
+            self.__frame += 1
+            if self.__frame == len(self.__sprite.frames):
+                self.__initiated = False
+                self.__frame = 0
+
+    def frame(self) -> str:
+        return self.__sprite.frame(self.__frame)
---
+++ test/test_game_state_element_animation.py
@@ -0,0 +1,56 @@
+from pix.game.state import Element, GameState
+from pix.sprite import Sprite
+
+def test_element_has_first_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png']))
+    assert game.element.frame() == 'frame1.png'
+
+def test_initiated_element_has_second_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png']))
+    game.element.initiate()
+    game.tick()
+    assert game.element.frame() == 'frame2.png'
+
+def test_initiated_element__after_game_ticks_twice__has_third_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png', 'frame3.png']))
+    game.element.initiate()
+    game.tick()
+    game.tick()
+    assert game.element.frame() == 'frame3.png'
+
+def test_not_initiated_element__after_game_ticks__has_first_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png']))
+    game.tick()
+    assert game.element.frame() == 'frame1.png'
+
+def test_initiated_element__after_last_frame__has_first_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png', 'frame3.png']))
+    game.element.initiate()
+    game.tick()
+    game.tick()
+    game.tick()
+    assert game.element.frame() == 'frame1.png'
+
+def test_initiated_element__after_last_frame__when_game_ticks__has_first_frame():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png', 'frame3.png']))
+    game.element.initiate()
+    game.tick()
+    game.tick()
+    game.tick()
+    game.tick()
+    assert game.element.frame() == 'frame1.png'
+
+def test_initiated_element__after_initiating_again__does_not_cancel_animation():
+    game = GameState()
+    game.element = Element(Sprite(['frame1.png', 'frame2.png', 'frame3.png']))
+    game.element.initiate()
+    game.tick()
+    game.element.initiate()
+    game.tick()
+    assert game.element.frame() == 'frame3.png'
